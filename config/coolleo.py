3a0a1987ef10e6f613fd01046a62187b52889e27
drakezilla <dt.drake@gmail.com>
2025-06-14 17:42:51 +0200

    Removed and ignored env file. Added new configs vars

--- config/coolleo.py
+++ config/coolleo.py
@@ -2,11 +2,15 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
-env_path = Path(__file__).parent.parent / ".env"
+root_path = Path(__file__).parent.parent
+env_path = root_path / ".env"
+resource_path = root_path / "resources/"
+
 load_dotenv(dotenv_path=env_path)
 
-
-default_mode = os.getenv("DEFAULT_MODE", "temperature")
-default_brightness = os.getenv("DEFAULT_BRIGHTNESS", 5)
-default_baudrate = os.getenv("DEFAULT_BAUDRATE", 9600)
-default_refresh_interval = int(os.getenv("DEFAULT_REFRESH_INTERVAL", 2))
+mode = os.getenv("DEFAULT_MODE", "temperature")
+brightness = os.getenv("DEFAULT_BRIGHTNESS", 5)
+baudrate = os.getenv("DEFAULT_BAUDRATE", 9600)
+refresh_interval = int(os.getenv("DEFAULT_REFRESH_INTERVAL", 2))
+socket_path = os.getenv("DEFAULT_SOCKET_PATH", "/tmp/coolleo_socket")
+show_device_communication = os.getenv("SHOW_DEVICE_COMM", "True").lower() in ("1", "true", "yes")
